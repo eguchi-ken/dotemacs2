228d18508a2e80a3db0cf27e8eeb8b672ea037cd
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2019-04-05 10:50:34 +0900

    Add file-full-path

--- init.el
+++ init.el
@@ -158,6 +158,20 @@
         (shell-command-to-string "echo -n $(LANG=ja_JP date +'%Y/%m/%d (%a)')")))
 (key-chord-define-global "id" 'insert-current-date)
 
+;; 今開いているファイルのフルパスを得る。dired バッファは NG
+(defun file-full-path ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
+
+
 
 ; for emacs cocoa
 (menu-bar-mode t)

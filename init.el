b655a0462c70d0141748c4607336710c35f875fe
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2019-05-30 15:14:23 +0900

    Add latest package source

--- init.el
+++ init.el
@@ -54,6 +54,7 @@
 
 (package-initialize)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-refresh-contents)
 (defvar my/favorite-packages '(use-package magit key-chord rebecca-theme wdired slim-mode coffee-mode wgrep dashboard projectile auto-complete idomenu ido-vertical-mode rubocop rbenv yasnippet))
 (dolist (package my/favorite-packages)

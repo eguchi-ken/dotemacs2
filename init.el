478110a62c56737be37f15e628a49943d90d2f69
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2019-07-29 13:44:55 +0900

    Use string-inflection-ruby-style-cycle

--- init.el
+++ init.el
@@ -250,7 +250,7 @@
   (key-chord-define-global "y7" 'yas-insert-snippet)
   (key-chord-define-global "id" 'insert-current-date)
   (key-chord-define-global "fp" 'file-full-path)
-  (key-chord-define-global "cy" 'string-inflection-cycle)
+  (key-chord-define-global "cy" 'string-inflection-ruby-style-cycle)
 )
 
 (use-package paradox

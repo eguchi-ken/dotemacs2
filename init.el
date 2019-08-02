ea735f4d05c2d14a2f00d78d010f1a72d329c33d
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2019-08-02 10:32:55 +0900

    Install spaceline

--- init.el
+++ init.el
@@ -90,7 +90,7 @@
     rbenv yasnippet dumb-jump dired-subtree
     slim-mode string-inflection
     coffee-mode wgrep dashboard paradox
-    projectile projectile-rails
+    projectile projectile-rails spaceline
     use-package magit key-chord rebecca-theme wdired))
 (dolist (package my/favorite-packages)
   (unless (package-installed-p package)
@@ -109,7 +109,6 @@
   (local-set-key (kbd "r")     'wdired-change-to-wdired-mode) ; ファイル名編集
 ))
 
-
 (setq ido-max-window-height 0.75)
 (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
 (ido-vertical-mode 1)
@@ -120,40 +119,6 @@
     (set (make-local-variable 'tab-width) 2)
     (setq coffee-tab-width 2))
 (add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
-
-;; モードラインのカスタマイズ
-;; https://qiita.com/kai2nenobu/items/ddf94c0e5a36919bc6db
-(require 'cl-lib)
-(set 'eol-mnemonic-dos "(CRLF)")
-(set 'eol-mnemonic-unix "(LF)")
-(set 'eol-mnemonic-mac "(CR)")
-(set 'eol-mnemonic-undecided "(?)")
-(defun my-coding-system-name-mnemonic (coding-system)
-  (let* ((base (coding-system-base coding-system))
-         (name (symbol-name base)))
-    (cond ((string-prefix-p "utf-8" name) "U8")
-          ((string-prefix-p "utf-16" name) "U16")
-          ((string-prefix-p "utf-7" name) "U7")
-          ((string-prefix-p "japanese-shift-jis" name) "SJIS")
-          ((string-match "cp\\([0-9]+\\)" name) (match-string 1 name))
-          ((string-match "japanese-iso-8bit" name) "EUC")
-          (t "???")
-          )))
-(defun my-coding-system-bom-mnemonic (coding-system)
-  (let ((name (symbol-name coding-system)))
-    (cond ((string-match "be-with-signature" name) "[BE]")
-          ((string-match "le-with-signature" name) "[LE]")
-          ((string-match "-with-signature" name) "[BOM]")
-          (t ""))))
-(defun my-buffer-coding-system-mnemonic ()
-  "Return a mnemonic for `buffer-file-coding-system'."
-  (let* ((code buffer-file-coding-system)
-         (name (my-coding-system-name-mnemonic code))
-         (bom (my-coding-system-bom-mnemonic code)))
-    (format "%s%s" name bom)))
-(setq-default mode-line-mule-info
-              (cl-substitute '(:eval (my-buffer-coding-system-mnemonic))
-                             "%z" mode-line-mule-info :test 'equal))
 
 (setq ruby-insert-encoding-magic-comment nil)
 (setq ruby-deep-indent-paren-style nil)
@@ -266,6 +231,11 @@
   (key-chord-define-global "id" 'insert-current-date)
   (key-chord-define-global "fp" 'file-full-path)
   (key-chord-define-global "cy" 'string-inflection-ruby-style-cycle)
+)
+
+(use-package spaceline-config
+  :config
+  (spaceline-emacs-theme)
 )
 
 (use-package paradox

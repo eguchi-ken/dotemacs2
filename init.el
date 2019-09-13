4e3ea609d9956f36607872e3aa5040866aacf8fb
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2019-09-13 16:34:55 +0900

    Add ignore file

--- init.el
+++ init.el
@@ -182,7 +182,7 @@
   (setf wgrep-enable-key "r")
   (setq wgrep-auto-save-buffer t)
   (setq my-ignore-directories '("node_modules" ".bundle" "yardoc" "coverage" "log" "tmp"))
-  (setq my-ignore-files '("*.min.js" "*.log"))
+  (setq my-ignore-files '("*.min.js" "*.log" "*bundle.js"))
   (setq grep-find-ignored-directories (append grep-find-ignored-directories my-ignore-directories))
   (setq grep-find-ignored-files (append grep-find-ignored-files my-ignore-files))
 )

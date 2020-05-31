a68eaa0c917a12a246433e065c2fe1d7fb1ec556
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-05-31 22:39:53 +0900

    package-refresh-contents は起動を遅くするので削除

--- init.el
+++ init.el
@@ -64,7 +64,6 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
 (defvar my/favorite-packages
   '(auto-complete rubocop direnv
     rbenv dired-subtree ivy counsel

9a79a9ab5ba849d980963b0d62a1bebcaa045c41
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-05-31 23:46:34 +0900

    Create package from custom functions

--- init.el
+++ init.el
@@ -40,6 +40,7 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
+(straight-use-package '(eguchi-ken :type git :host github :repo "eguchi-ken/eguchi-ken.el"))
 
 (add-hook 'org-mode-hook (lambda ()
   (setq org-agenda-files '("~/work/text/report.org"))
@@ -113,44 +114,6 @@
 (defun ~ruby-fix-syntax-propertize ()
   (add-function :before (local 'syntax-propertize-function) '~ruby-syntax-propertize-function))
 (add-hook 'ruby-mode-hook '~ruby-fix-syntax-propertize t)
-
-(defun insert-current-date (&optional diff)
-  "日にちをカレントバッファに出力します"
-  (interactive "P")
-    (insert
-     (shell-command-to-string
-      (format
-       "echo -n $(LANG=ja_JP date -v-%dd +'%%Y/%%m/%%d (%%a)')"
-       (or diff 0)))))
-
-(defun file-full-path ()
-  "今開いているファイルの絶対パス::行数を返します"
-  (if (equal major-mode 'dired-mode)
-      default-directory
-    (concat (buffer-file-name) "::" (number-to-string (line-number-at-pos)))))
-
-(defun to-clipboard (x)
-  "与えられた文字列をクリップボードにコピーします"
-  (when x
-    (with-temp-buffer
-      (insert x)
-      (clipboard-kill-region (point-min) (point-max)))
-    (message x)))
-
-(defun file-full-path-to-clipboard ()
-  "今開いているファイルの org link をクリップボードにコピーします"
-  (interactive)
-  (to-clipboard (file-full-path)))
-
-(defun file-full-path-org-link-to-clipboard ()
-  "今開いているファイルの org link をクリップボードにコピーします"
-  (interactive)
-  (to-clipboard (concatenate 'string "[[" (file-full-path) "][" (file-name-nondirectory buffer-file-name) "]]")))
-
-(defun open-current-buffer-file ()
-  "今開いているファイルを open します"
-  (interactive)
-  (shell-command (concat "open " (buffer-file-name))))
 
 ; for emacs cocoa
 (menu-bar-mode t)
@@ -372,27 +335,6 @@
 
 (use-package go-mode
   :config
-  (add-hook 'go-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode 1)
-            (setq tab-width 2))))
-
-(defun replace-org-to-markdown ()
-  (interactive)
-  (save-excursion
-    (let (replacement)
-      (setq replacement
-            '("^* "                                "# "
-              "^** "                               "## "
-              "^*** "                              "### "
-              "^**** "                             "#### "
-              "^***** "                            "##### "
-              "#\\+BEGIN_SRC"                      "```"
-              "#\\+END_SRC"                        "```"
-              "\\[\\[\\(.+\\)\\]\\[\\(.+\\)\\]\\]" "[\\2](\\1)"))
-      (while replacement
-        (goto-char (point-min))
-        (replace-regexp (pop replacement) (pop replacement))))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
